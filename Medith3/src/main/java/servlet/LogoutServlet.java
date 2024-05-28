f6494f8d58cf20152310505c19e25c7cd832b52f
Administrator <Administrator@PC-number>
2024-05-28 10:56:13 +0900

    ログイン・ログアウトhtml

--- Medith3/src/main/java/servlet/LogoutServlet.java
+++ Medith3/src/main/java/servlet/LogoutServlet.java
@@ -17,7 +17,7 @@ public class LogoutServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.invalidate();
-        response.sendRedirect("login.html");
+        response.sendRedirect("logout.html");
     }
 
     @Override
